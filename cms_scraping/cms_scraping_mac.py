8d90ec296442816c800b50b2ba7147293df815cc
cysk003 <cysk003@126.com>
2024-04-03 05:11:29 +0800

    Update cms_scraping_mac.py

--- cms_scraping/cms_scraping_mac.py
+++ cms_scraping/cms_scraping_mac.py
@@ -1,5 +1,6 @@
 import requests
 import logging
+import time
 
 # 设置日志记录
 logging.basicConfig(filename='app.log', level=logging.INFO, format='%(asctime)s - %(message)s')
@@ -8,7 +9,14 @@ def get_data_from_api(host):
     base_url = f'{host}/api.php/provide/vod/?ac=list&ac=videolist&t=&pg={{}}&h=&ids=&wd='
 
     try:
-        response = requests.get(base_url.format(1))
+        response = requests.get(base_url.format(1), verify=False)
+        if response.status_code == 200:
+            print("第一次请求成功")
+        else:
+            print(f"第一次请求失败，状态码: {response.status_code}")
+            logging.error(f"第一次请求失败，状态码: {response.status_code}")
+            return
+
         response.raise_for_status()  # 如果请求失败，会抛出异常
 
         data = response.json()
@@ -20,7 +28,14 @@ def get_data_from_api(host):
 
         for page in range(1, pagecount + 1):
             url = base_url.format(page)
-            response = requests.get(url)
+            response = requests.get(url, verify=False)
+            if response.status_code == 200:
+                print(f"第{page}次请求成功")
+            else:
+                print(f"第{page}次请求失败，状态码: {response.status_code}")
+                logging.error(f"第{page}次请求失败，状态码: {response.status_code}")
+                continue
+
             response.raise_for_status()
 
             data = response.json()
@@ -48,10 +63,14 @@ def get_data_from_api(host):
             progress = (page / pagecount) * 100
             print(f"当前进度: {page}/{pagecount} ({progress:.2f}%)")
 
+            time.sleep(5)  # 每次请求后休眠5秒
+
     except requests.RequestException as e:
         logging.error(f"请求失败: {str(e)}")
+        print(f"请求失败: {str(e)}")
     except (KeyError, ValueError) as e:
         logging.error(f"数据解析错误: {str(e)}")
+        print(f"数据解析错误: {str(e)}")
 
     print("结果已保存在result.txt文件中")
 
