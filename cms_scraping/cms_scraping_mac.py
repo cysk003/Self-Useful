dc23dd071f4b0165b421052def0e1112d33d5e06
cysk003 <cysk003@126.com>
2024-04-03 05:59:23 +0800

    Update cms_scraping_mac.py

--- cms_scraping/cms_scraping_mac.py
+++ cms_scraping/cms_scraping_mac.py
@@ -2,22 +2,39 @@ import requests
 import logging
 import time
 
-# 设置日志记录
 logging.basicConfig(filename='app.log', level=logging.INFO, format='%(asctime)s - %(message)s')
 
 def get_data_from_api(host):
     base_url = f'{host}/api.php/provide/vod/?ac=list&ac=videolist&t=&pg={{}}&h=&ids=&wd='
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36'
+    }
+
+    def process_item(item):
+        vod_name = item.get('vod_name')
+        type_name = item.get('type_name')
+        vod_play_url = item.get('vod_play_url')
+        
+        vod_name_with_type = f"[{type_name}] {vod_name}"
+        vod_play_url = vod_play_url.replace('$', ',')
+        result = f"{vod_name_with_type} {vod_play_url}"
+
+        print(result)
+        logging.info(result)
+
+        with open('result.txt', 'a') as file:
+            file.write(result + '\n')
 
     try:
-        response = requests.get(base_url.format(1), verify=False)
+        response = requests.get(base_url.format(1), headers=headers, verify=True)
+        
         if response.status_code == 200:
-            print("第一次请求成功")
+            print("第一次请求成功！")
         else:
             print(f"第一次请求失败，状态码: {response.status_code}")
-            logging.error(f"第一次请求失败，状态码: {response.status_code}")
             return
 
-        response.raise_for_status()  # 如果请求失败，会抛出异常
+        response.raise_for_status()
 
         data = response.json()
         pagecount = data.get('pagecount')
@@ -28,14 +45,7 @@ def get_data_from_api(host):
 
         for page in range(1, pagecount + 1):
             url = base_url.format(page)
-            response = requests.get(url, verify=False)
-            if response.status_code == 200:
-                print(f"第{page}次请求成功")
-            else:
-                print(f"第{page}次请求失败，状态码: {response.status_code}")
-                logging.error(f"第{page}次请求失败，状态码: {response.status_code}")
-                continue
-
+            response = requests.get(url, headers=headers, verify=True)
             response.raise_for_status()
 
             data = response.json()
@@ -46,34 +56,39 @@ def get_data_from_api(host):
                 continue
 
             for item in vod_list:
-                vod_name = item.get('vod_name')
-                type_name = item.get('type_name')
-                vod_play_url = item.get('vod_play_url')
-                vod_play_url = vod_play_url.split('$')[-1]
-
-                vod_name_with_type = f"[{type_name}] {vod_name}"
-                result = f"{vod_name_with_type},{vod_play_url}"
-
-                print(result)
-                logging.info(result)
-
-                with open('result.txt', 'a') as file:
-                    file.write(result + '\n')
+                process_item(item)
 
             progress = (page / pagecount) * 100
             print(f"当前进度: {page}/{pagecount} ({progress:.2f}%)")
-
-            time.sleep(5)  # 每次请求后休眠5秒
+            time.sleep(5)
 
     except requests.RequestException as e:
-        logging.error(f"请求失败: {str(e)}")
-        print(f"请求失败: {str(e)}")
-    except (KeyError, ValueError) as e:
+        logging.error(f"请求错误: {str(e)}")
+        print(f"请求错误: {str(e)}")
+    except ValueError as e:
         logging.error(f"数据解析错误: {str(e)}")
         print(f"数据解析错误: {str(e)}")
+    except Exception as e:
+        logging.error(f"发生未知错误: {str(e)}")
+        print(f"发生未知错误: {str(e)}")
 
     print("结果已保存在result.txt文件中")
 
 if __name__ == "__main__":
-    host = input("请输入主机名（默认为http://www.9191md.me）: ") or 'http://www.9191md.me'
+    hosts = {
+        1: 'http://www.9191md.me',
+        2: 'https://apiyutu.com'
+    }
+
+    print("目前支持的主机名选项：")
+    for key, value in hosts.items():
+        print(f"{key}: {value}")
+
+    choice = input("请输入主机名代号（默认为1）: ")
+
+    if choice.strip() == '':
+        host = 'http://www.9191md.me'
+    else:
+        host = hosts.get(int(choice), 'http://www.9191md.me')
+
     get_data_from_api(host)
