8d7322be8bf75b3191f600933955e89c3afeeba3
cysk003 <cysk003@126.com>
2024-04-05 15:58:43 +0800

    Update video_list_download.py

--- iptvscan/video_list_download.py
+++ iptvscan/video_list_download.py
@@ -20,6 +20,7 @@ def main():
     input_link = input("请输入一个链接进行访问：")
     
     # 获取数据
+    print(f"正在访问链接：{input_link}")
     data = download_data(input_link)
     if not data:
         print("无法获取数据。")
@@ -34,14 +35,17 @@ def main():
 
     # 获取所有url
     urls = [value for key, value in json_data.items() if key == 'url']
+    print(f"从JSON数据中提取到的URL列表：{urls}")
     
     # 遍历url，下载数据并保存到本地
     for url in urls:
         try:
+            print(f"正在访问链接：{url}")
             response = requests.get(url)
             if response.status_code == 200:
                 # 获取文件名
                 filename = os.path.basename(urlparse(url).path)
+                print(f"文件名：{filename}")
                 # 删除已存在的文件
                 if os.path.exists(filename):
                     os.remove(filename)
